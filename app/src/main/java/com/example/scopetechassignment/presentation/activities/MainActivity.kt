a08d439b2a70dcb90cdcb8863f4ccbe73140275f
raveena <raveena.deshmane@nice.com>
2022-09-27 00:47:22 +0300

    Implemented compose Ui for rendering list on the home screen.

--- app/src/main/java/com/example/scopetechassignment/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/scopetechassignment/presentation/activities/MainActivity.kt
@@ -2,51 +2,87 @@ package com.example.scopetechassignment.presentation.activities
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.example.scopetechassignment.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.scopetechassignment.data.models.Data
+import com.example.scopetechassignment.data.models.Owner
 import com.example.scopetechassignment.domain.Status
+import com.example.scopetechassignment.presentation.util.collectLatestLifecycleFlow
 import com.example.scopetechassignment.presentation.viewmodels.GetUserListViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel: GetUserListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        collectLatestLifecycleFlow(viewModel.userDetailState) {
-            when (it.status) {
-                Status.LOADING -> {
-                    //Do nothing
-                }
-                Status.ERROR -> {
-                    Toast.makeText(this@MainActivity, it.errorMessage, Toast.LENGTH_LONG).show()
-                }
-                Status.SUCCESS -> {
-                    it.data?.let { data ->
-                        Toast.makeText(
-                            this@MainActivity,
-                            "size : ${data.userData.size}",
-                            Toast.LENGTH_LONG
-                        ).show()
+        setContent {
+            val userResponseState = remember { mutableStateOf<List<Data>>(emptyList()) }
+            collectLatestLifecycleFlow(viewModel.userDetailState) {
+                when (it.status) {
+                    Status.LOADING -> {
+                        //Do nothing
+                    }
+                    Status.ERROR -> {
+                        Toast.makeText(this@MainActivity, it.errorMessage, Toast.LENGTH_LONG).show()
+                    }
+                    Status.SUCCESS -> {
+                        it.data?.let { data ->
+                            userResponseState.value = data.userData
+                        }
                     }
                 }
             }
+            UserList(userList = userResponseState.value)
         }
     }
 }
 
-fun <T> AppCompatActivity.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
-    lifecycleScope.launch {
-        repeatOnLifecycle(Lifecycle.State.STARTED) {
-            flow.collectLatest(collect)
+@Composable
+fun UserRow(
+    modifier: Modifier = Modifier,
+    user: Owner
+) {
+    Row {
+        Image(
+            painter = rememberAsyncImagePainter(user.photo),
+            modifier = modifier
+                .width(40.dp)
+                .height(40.dp)
+                .align(Alignment.CenterVertically)
+                .padding(10.dp),
+            contentDescription = "Profile Image"
+        )
+        Text(
+            text = "${user.name} ${user.surname}",
+            modifier = modifier.wrapContentSize()
+        )
+
+    }
+
+}
+
+@Composable
+fun UserList(modifier: Modifier = Modifier, userList: List<Data>) {
+    if (userList.isNotEmpty()) {
+        LazyColumn(modifier = modifier.fillMaxSize()) {
+            items(userList) { user ->
+                UserRow(modifier, user.owner)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/scopetechassignment/presentation/util/Extensions.kt
@@ -0,0 +1,20 @@
+package com.example.scopetechassignment.presentation.util
+
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+fun <T> AppCompatActivity.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
+        }
+    }
+}
+
